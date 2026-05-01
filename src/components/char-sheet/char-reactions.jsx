6537c3f78fa7d748ff5bf02ae074e0e80468a20e
paulgilchrist <PGilchrist@pulte.com>
2026-05-01 10:59:29 -0400

    fix: stop mutating props directly in char-reactions and char-special-actions
    - Create local copies of playerStats.reactions and playerStats.specialActions
    - Use spread operator to avoid direct mutation of props
    - All push/find/some/map operations now work on local copies instead of props

--- src/components/char-sheet/char-reactions.jsx
+++ src/components/char-sheet/char-reactions.jsx
@@ -8,13 +8,17 @@ function CharReactions({ playerStats }) {
         if (specialAction.details) {
             let html = `<b>${specialAction.name}</b><br/>${specialAction.description}<br/><br/>${specialAction.details}`;
             setPopupHtml(html);
-        }
     }
+      }
+
+     // Build reactions list immutably
+    let reactions = [...(playerStats.reactions || [])];
+
     if (playerStats.spellAbilities && playerStats.spellAbilities.spells.length > 0) {
         let reactionSpells = playerStats.spellAbilities.spells.filter(spell => spell.casting_time === '1 reaction' && (spell.prepared === 'Always' || spell.prepared === 'Prepared'));
         reactionSpells.forEach(spell => {
-            if (!playerStats.reactions.some((reaction) => reaction.name === spell.name)) {
-                playerStats.reactions.push({
+            if (!reactions.some((reaction) => reaction.name === spell.name)) {
+                reactions.push({
                     "name": spell.name,
                     "description": spell.desc
                 });
@@ -22,14 +26,14 @@ function CharReactions({ playerStats }) {
         });
     }
     // Add base reactions to reaction list
-    if (!playerStats.reactions.find((reaction) => reaction.name === 'Opportunity Attack')) {
-        playerStats.reactions.push({ "name": "Opportunity Attack", "description": "Can attack creature that moves out of your reach" });
+    if (!reactions.find((reaction) => reaction.name === 'Opportunity Attack')) {
+        reactions.push({ "name": "Opportunity Attack", "description": "Can attack creature that moves out of your reach" });
     }
 
     return (
         <div>
             <div className='sectionHeader'>Reactions</div>
-            {playerStats.reactions.map((reaction) => {
+              {reactions.map((reaction) => {
                 return <div key={reaction.name}>
                     {popupHtml && (<Popup html={popupHtml} onClickOrKeyDown={() => setPopupHtml(null)}></Popup>)}
                     <b className={reaction.details ? "clickable" : ""} onClick={() => showPopup(reaction)}>{reaction.name}:</b> <span dangerouslySetInnerHTML={{ __html: reaction.description }}></span>
@@ -40,3 +44,4 @@ function CharReactions({ playerStats }) {
 }
 
 export default CharReactions
+
